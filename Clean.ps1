416f4229a97c975f1a4743da1982d21eb5c1a5f7
Chechulin Sergey <keefeere@ukr.net>
2021-07-30 17:10:28 +0300

    pass setup

---
+++ Clean.ps1
@@ -0,0 +1,35 @@
+﻿$servers = import-csv -path ".\servers.csv" -Delimiter ";"
+$cleanCommands = ".\cleancommands.ps1"
+$PSExec = "C:\Windows\System32\PSExec.exe"
+
+
+
+Write-Host 'Enabling WinRM on local machine'
+Enable-PSRemoting -Force
+Restart-Service WinRM
+#Start-Process -Filepath "winrm" -ArgumentList "quickconfig" -Wait
+
+Foreach($server in $servers){
+
+    Write-Host ('Cleaning server '+$($server.servername))
+
+    $password = ConvertTo-SecureString $($server.pass) -AsPlainText -Force
+    $Creds = New-Object System.Management.Automation.PSCredential ($($server.user), $password)
+    #"добавить компьютер назначения к значениям параметра конфигурации TrustedHosts. "
+    Set-Item WSMan:\localhost\Client\TrustedHosts -Value $($server.servername) -Force
+
+    #Включить WinRM
+    Write-Host 'Enabling WinRM on remote host with psexec'
+    Start-Process -Filepath "$PSExec" -ArgumentList "\\$($server.servername) -u $($server.user) -p $($server.pass) -h -s  -accepteula -nobanner powershell.exe Enable-PSRemoting –SkipNetworkProfileCheck -Force; Set-NetFirewallRule -Name 'WINRM-HTTP-In-TCP' -RemoteAddress Any"-NoNewWindow -Wait
+
+    
+    Write-Host 'Open session on remote host'
+    $session = New-PSSession -ComputerName $($server.servername) -Credential $Creds
+    
+    Write-Host 'Invoking commands on remote host '
+    $result = Invoke-Command -Session $session -FilePath $cleanCommands
+    Write-Host $result
+    
+  
+
+}
---
+++ cleancommands.ps1
@@ -0,0 +1,38 @@
+#logoff all users except current
+ForEach ($session in (query user $_ | where-object {($_ -notmatch 'console') -and ($_ -notmatch 'services') -and ($_ -notmatch $env:USERNAME) -and ($_ -notmatch '65536') -and ($_ -notmatch 'ID')})){
+    #$sessionstr= $session -split '\s+'
+    logoff ($session -split '\s+')[3]#session id 
+}
+
+
+Remove-Item C:\Windows\Temp\* -Recurse -Force
+Remove-Item c:\shells*\* -Recurse -Force
+Remove-Item C:\XuanZhi*\LDPlayer\* -Recurse -Force
+Remove-Item C:\ChangZhi*\LDPlayer\* -Recurse -Force
+Remove-Item C:\ledian*\LDPlayer\* -Recurse -Force
+
+
+Remove-Item D:\XuanZhiU*\ -Recurse -Force
+Remove-Item D:\ChangZhiU*\ -Recurse -Force
+Remove-Item D:\ledianU*\ -Recurse -Force
+
+Remove-Item C:\temp\* -Recurse -Force
+Remove-Item C:\sesman_shell -Recurse -Force
+
+Remove-Item 'c:\$Recycle.Bin\*' -Recurse -Force
+
+
+$ip = (Get-WmiObject Win32_NetworkAdapterConfiguration -Filter 'IPEnabled=True').IPAddress[0]
+$lastOctet = $ip.Split('.')[-1]
+$Password = "newPWD~~$lastOctet"
+
+net user Administrator $Password
+net user u1 $Password
+net user u2 $Password
+net user u3 $Password
+net user u5 $Password
+net user u6 $Password
+net user u7 $Password
+net user u8 $Password
+
+Get-ChildItem -File -Recurse -Path C:\Fileshare\ | %{ Remove-Item -Path $_.FullName -force }
